45ea077ba9f25a0d778030f2a4dc25adc74451ba
conrad mugabe <conradmugabe@gmail.com>
2023-07-03 00:30:20 +0300

    feat: added testing to application

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+import path from "path";
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: { port: 3000 },
+  resolve: {
+    alias: {
+      "@src": path.resolve(__dirname, "/src"),
+    },
+  },
 });
